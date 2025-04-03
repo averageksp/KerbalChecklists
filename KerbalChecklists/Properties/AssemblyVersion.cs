e796408b822383bd810e95ca95ede76c5df3df3f
averageksp <158779203+averageksp@users.noreply.github.com>
2025-04-03 16:00:51 +0200

    Create AssemblyVersion.cs

---
+++ KerbalChecklists/Properties/AssemblyVersion.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KerbalChecklists")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KerbalChecklists")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c8efc0fa-de75-4aa1-b3c1-d1553a0340e9")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
